7125b8c2693130582c40894b3804f325309991cf
Lucas Webb <lucasawebb@gmail.com>
2020-02-14 16:17:28 -0700

    Many Changes

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -5,6 +5,7 @@ This program is a fun and exciting Tic Tac Toe game
 */
 
 #include <vector>
+#include <iostream>
 using namespace std;
 
 vector<vector<int> > CreateBoard(){
@@ -17,8 +18,11 @@ void DisplayBoard(vector<vector<int> > board){
         for(int j = 0; j < 3; j++){
             switch(board[i][j]){
                 case 0: cout << "~";
+                    break;
                 case 1: cout << "X";
+                    break;
                 case 2: cout << "O";
+                    break;
                 default: break;
             }
         }
@@ -30,7 +34,7 @@ int GetPlayerChoice(){
   cout << "Where would you like to move?(Enter a number 0 - 8)" << endl;
   string user_response;
   getline(cin,user_response);
-  return user_response
+  return stoi(user_response);
 }
     
 void PlaceMarker(int location, int mark, vector<vector<int> > &board){
@@ -41,5 +45,18 @@ void PlaceMarker(int location, int mark, vector<vector<int> > &board){
 
 int main(){
     vector<vector<int> > board = CreateBoard();
+
+    for(int i = 0; i < 9; i++){
+        int marker;
+        DisplayBoard(board);
+        int move = GetPlayerChoice();
+        if(i%2){
+            marker = 1;
+        } else {
+            marker = 2;
+        }
+        PlaceMarker(move, marker, board);
+    }
+    DisplayBoard(board);
     return 1;
 }
