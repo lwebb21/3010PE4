0203de25ede1acfbdbcfc58973f790f67952a953
Rebecca Carr <47703707+Birdfeathers@users.noreply.github.com>
2020-02-14 15:41:31 -0700

    Update TicTacToe.cpp

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -4,11 +4,15 @@ PE 4
 This program is a fun and exciting Tic Tac Toe game
 */
 
-void CreateBoard(){
+#include <vector>
+using namespace std;
+
+vector<vector<int> > CreateBoard(){
     vector<vector<int> > board{{0, 0 , 0}, {0, 0 , 0}, {0, 0 ,0}};
+    return board;
 }
 
 int main(){
-    CreateBoard();
+    vector<vector<int> > board = CreateBoard();
     return 1;
 }
